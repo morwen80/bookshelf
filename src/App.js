3067d3125f40c2a5e9d8b55ed512ae2768ab58bf
morwen80 <ribesnero80@gmail.com>
2019-04-28 10:20:28 +0200

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      title:  '',
+      author: ''
+    }
+  }
+
   render() {
     return (
       <div>
-        Mamma culo
+        <Header />
       </div>
     )
   }
 }
+
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <h1>Bookshelf</h1>
+    <h3>your personal library</h3>
+  </div>
+)
+
+export default Header
